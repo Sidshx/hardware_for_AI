911b62e675cf5c6ffd8b86ffe37ee8b95a30e629
Sidxsh <sidsh@pop-os.localdomain>
2025-06-15 19:11:41 -0700

    Updating directory

--- proj/README.md
+++
@@ -1,2 +0,0 @@
-https://docs.google.com/document/d/1dm9R3V2YBzzBpP9iOklFmMPIsP861DoNFOufaAKt0Wo/edit?tab=t.nvnxyfj9bjps
-
--- proj/cocotb/sim_build_comprehensive/iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module iverilog_dump();
-initial begin
-    $dumpfile("viterbi_top.fst");
-    $dumpvars(0, viterbi_top);
-end
-endmodule
